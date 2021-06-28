617888e8008c824963cf9cdcbcde0885791f9b69
Ivan <bozkovana@gmail.com>
2021-06-28 12:46:12 +0300

    Add template for nav page 'Contacts'

--- root_art/main/views.py
+++ root_art/main/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title': 'Главная страница',
+        'values': ['Some', 'Hello', 228]
+    }
+    return render(request, 'main/index.html', data)
 
 
 def about(request):
     return render(request, 'main/about.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
